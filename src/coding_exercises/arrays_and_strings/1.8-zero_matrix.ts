cde9e85a95fc146a21342220d0000b7008fbfecd
Eric So <me@eric.so>
2025-05-22 12:27:31 -0700

    stubs for 1.8 and 1.9

---
+++ src/coding_exercises/arrays_and_strings/1.8-zero_matrix.ts
@@ -0,0 +1,11 @@
+/**
+ 1.8 Zero Matrix:
+ Write an algorithm such that if an element in an MXN matrix is 0, its entire row and column are set to 0.
+ Hints: #17, #74, #102
+ pg 204
+
+ Source: Cracking the Coding Interview, 6th Edition, page 91, question 1.8
+
+ Discussion
+
+ */
